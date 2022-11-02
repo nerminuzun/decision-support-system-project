24ba9cfeac69ad827bf0939b65e0bb7bbd061ef4
nerminuzun <77100804+nerminuzun@users.noreply.github.com>
2022-11-02 19:48:28 +0300

    Create log.php

---
+++ log.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="tr">
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lezzet Şöleni</title>
+    <link rel="stylesheet" href="log.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script>
